6326b735e5d08ff5a7e2654978bbd1586c66b1f5
Matyas Krista <matyas@geomiq.com>
2021-05-23 16:22:58 +0100

    RestaurantList tests

--- src/components/RestaurantList.vue
+++ src/components/RestaurantList.vue
@@ -1,16 +1,29 @@
 <template>
-  <div></div>
+  <ul>
+    <li
+      data-testid="restaurant"
+      v-for="restaurant in restaurants"
+      :key="restaurant.id"
+    >
+      {{ restaurant.name }}
+    </li>
+  </ul>
 </template>
 
 <script lang="ts">
 import {Component, Vue} from 'vue-property-decorator';
-import {Action} from 'vuex-class';
+import {namespace} from 'vuex-class';
 import {Restaurant} from '@/types/Restaurant';
+
+const restaurantsModule = namespace('restaurants');
 
 @Component
 export default class RestaurantList extends Vue {
-  @Action('restaurants/load')
-  loadRestaurants!: () => Restaurant[];
+  @restaurantsModule.Action('load')
+  loadRestaurants!: () => {data: Restaurant[]; status: number};
+
+  @restaurantsModule.State('records')
+  restaurants: Restaurant[];
 
   mounted(): void {
     this.loadRestaurants();
--- src/types/Restaurant.ts
+++ src/types/Restaurant.ts
@@ -1,3 +1,4 @@
 export interface Restaurant {
+    id: number,
     name: string,
 }
