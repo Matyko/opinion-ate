1ecc8976586b746c34357fb0c225fbcb4577ae41
Matyas Krista <matyas@geomiq.com>
2021-05-23 16:08:28 +0100

    Load restaurants on mount

--- src/components/RestaurantList.vue
+++ src/components/RestaurantList.vue
@@ -4,9 +4,18 @@
 
 <script lang="ts">
 import {Component, Vue} from 'vue-property-decorator';
+import {Action} from 'vuex-class';
+import {Restaurant} from '@/types/Restaurant';
 
 @Component
-export default class RestaurantList extends Vue {}
+export default class RestaurantList extends Vue {
+  @Action('restaurants/load')
+  loadRestaurants!: () => Restaurant[];
+
+  mounted(): void {
+    this.loadRestaurants();
+  }
+}
 </script>
 
 <style scoped></style>
