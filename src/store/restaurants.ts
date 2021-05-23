355c562c4420e990e506664a2d2363273639cdb6
Matyas Krista <matyas@geomiq.com>
2021-05-23 19:51:31 +0100

    Load tests and consts

--- src/store/restaurants.ts
+++ src/store/restaurants.ts
@@ -4,23 +4,38 @@ import {RootState} from '@/store/index';
 import {Api} from '@/api';
 
 export type RestaurantState = {
-  records: Restaurant[];
+  [CONSTANTS.RECORDS]: Restaurant[];
+  [CONSTANTS.LOADING]?: boolean;
 };
+
+export enum CONSTANTS {
+  STORE_RECORDS = 'STORE_RECORDS',
+  SET_LOADING = 'SET_LOADING',
+  LOAD = 'LOAD',
+  RECORDS = 'RECORDS',
+  LOADING = 'LOADING',
+}
 
 const restaurants: (api: Api) => Module<RestaurantState, RootState> = api => ({
   namespaced: true,
   state: {
-    records: [],
+    [CONSTANTS.RECORDS]: [],
+    [CONSTANTS.LOADING]: true,
   },
   mutations: {
-    storeRecords(state, records) {
-      state.records = records;
+    [CONSTANTS.STORE_RECORDS](state, records) {
+      state[CONSTANTS.RECORDS] = records;
+    },
+    [CONSTANTS.SET_LOADING](state, loading) {
+      state[CONSTANTS.LOADING] = loading;
     },
   },
   actions: {
-    load({commit}) {
+    [CONSTANTS.LOAD]({commit}) {
+      commit(CONSTANTS.SET_LOADING, true);
       api.loadRestaurants().then((records: Restaurant[]) => {
-        commit('storeRecords', records);
+        commit(CONSTANTS.STORE_RECORDS, records);
+        commit(CONSTANTS.SET_LOADING, false);
       });
     },
   },
--- tests/unit/store/restaurants.spec.ts
+++ tests/unit/store/restaurants.spec.ts
@@ -1,15 +1,16 @@
 import Vuex from 'vuex';
 import {createLocalVue} from '@vue/test-utils';
-import restaurants from '@/store/restaurants';
+import restaurants, {CONSTANTS} from '@/store/restaurants';
 import Vue from 'vue';
 import Vuetify from 'vuetify';
+import {Restaurant} from '@/types/Restaurant';
 
 describe('restaurants', () => {
   Vue.use(Vuetify);
   const localVue = createLocalVue();
   localVue.use(Vuex);
 
-  describe('load action', () => {
+  describe('when loading succeeds', () => {
     it('stores the restaurants', async () => {
       const records = [
         {id: 1, name: 'Sushi Place'},
@@ -26,8 +27,23 @@ describe('restaurants', () => {
         },
       });
 
-      await store.dispatch('restaurants/load');
-      expect(store.state.restaurants.records).toEqual(records);
+      await store.dispatch(`restaurants/${[CONSTANTS.LOAD]}`);
+      expect(store.state.restaurants[CONSTANTS.RECORDS]).toEqual(records);
+    });
+  });
+
+  describe('while loading', () => {
+    it('sets a loading flag', () => {
+      const api = {
+        loadRestaurants: (): Promise<Restaurant[]> => new Promise(() => ({})),
+      };
+      const store = new Vuex.Store({
+        modules: {
+          restaurants: restaurants(api),
+        },
+      });
+      store.dispatch(`restaurants/${[CONSTANTS.LOAD]}`);
+      expect(store.state.restaurants[CONSTANTS.LOADING]).toEqual(true);
     });
   });
 });
